3bb3b18e8158b7e3f3d6d5200ca747734f93c8e7
Lightop <lightop@yandex.ru>
2017-12-24 21:04:56 +0300

    Asyncio osc_server and Encoders

--- mqto.py
+++ mqto.py
@@ -1,11 +1,11 @@
 import argparse
 import math
 import pyautogui
-
 from pythonosc import dispatcher
 from pythonosc import osc_server
+import asyncio
 
-pyautogui.PAUSE = 0.1
+pyautogui.PAUSE = 0
 pyautogui.FAILSAFE = True
 PRESSED = 0
 POS = 724
@@ -16,12 +16,12 @@ def s_handler(unused_addr, args, volume):
   #a = round (724-90*volume)
   #pyautogui.moveTo (128,a+1)
   #pyautogui.dragTo (128,a)
-  print (args[0])
+  #print (args[0])
   pyautogui.click(args[0],559)
 
 def sb_handler(unused_addr,args,volume):
-  print (args[0])
-  print (PRESSED)
+  #print (args[0])
+  #print (PRESSED)
   pyautogui.click(args[0],105)
 
 def go_handler(unused_addr,args,volume):
@@ -29,10 +29,10 @@ def go_handler(unused_addr,args,volume):
     pyautogui.click(args[0], 586)
 
 def ft_handler (unused_addr, args,volume):
-  print (volume)
-  print ("***")
+  #print (volume)
+  #print ("***")
   if (volume == 1): 
-    pyautogui.mouseDown(128,724)
+    pyautogui.mouseDown(128,724-volume)
     print ("Down")
 
   if (volume ==0):
@@ -40,21 +40,43 @@ def ft_handler (unused_addr, args,volume):
     print ("Up")
 
 def f_handler (unused_addr, args,volume):
-  #print (volume)
-  if (volume == v):
-    exit
-    print ("UUU")
- # print ("------")
-  #pyautogui.moveTo(128,724-volume)
-  v = volume
-  #sleep (1)
-
+  pyautogui.moveTo(128,724-volume)
+  
 def b_handler (unused_addr,args,volume):
-  print (args[0], args[1])
+  #print (args[0], args[1])
   if volume == 1.0 :
     pyautogui.click (args[0], args[1])
-  print ("V:"+str(volume))
+  #print ("V:"+str(volume))
   
+def wb_handler (unused_addr,args,volume):
+  if volume == 1.0 :
+    pyautogui.click (120, args[0])
+    #print (args[0])
+
+def wt_handler (unused_addr,args,volume):
+  if (volume == 1.0):
+    try:
+      pyautogui.mouseDown (40,args[0])
+    except (RuntimeError,ValueError): pass
+  if (volume == 0.0):
+    try:
+      pyautogui.mouseUp()
+    except (RuntimeError,ValueError): pass
+
+def w_handler (unused_addr,args,volume):
+  if (volume == 1.0):
+    try:
+      #pyautogui.mouseDown (40,172)
+      pyautogui.moveRel(0,1)
+      #pyautogui.mouseUp()
+    except (RuntimeError,ValueError): pass
+  if (volume == 0.0):
+    try:
+        ##pyautogui.mouseDown(40,172)
+      pyautogui.moveRel(0,-1)
+      #pyautogui.mouseUp()
+    except (RuntimeError,ValueError): pass
+      
 
 
 
@@ -72,9 +94,7 @@ if __name__ == "__main__":
   args = parser.parse_args()
   dispatcher = dispatcher.Dispatcher()
   for x in range (0,9):
-      print ("/mq/s/"+str(x+1))
       y = 128+x*97
-      print (y)
       dispatcher.map("/mq/s/"+str(x+1), s_handler, y)
       dispatcher.map("/mq/fader/"+str(x+1)+"/z", ft_handler, y)
       dispatcher.map("/mq/fader/"+str(x+1), f_handler, y)
@@ -82,15 +102,21 @@ if __name__ == "__main__":
 
   for x in range (0,11):
       y = 128+x*80
-      print (y)
       dispatcher.map("/mq/sb/"+str(x+1), sb_handler,y)
-  #dispatcher.map("/logvolume", print_compute_handler, "Log volume", math.log)
+
+  for x in range (0,2):
+    y=150+x*40
+    dispatcher.map("/mq/wb/"+str(x+1),wb_handler,y)
+
+  for x in range (0,4):
+    y = 172+x*90
+    dispatcher.map("/mq/wheel/"+str(x+1),w_handler, y)
+    dispatcher.map("/mq/wheel/"+str(x+1)+"/z", wt_handler,y)
+
   i=1
   for x in range (0,5):
     for y in range (0,3):
       dispatcher.map("/mq/b/"+str(i), b_handler,1126+30*y,600+30*x)
-      print (1126+30*y, 600+30*x)
-      print (i)
       i = i+1
   
   i=16
@@ -117,7 +143,8 @@ if __name__ == "__main__":
       dispatcher.map("/mq/b/"+str(i), b_handler, 1165+30*y, 245+30*x)
       i=i+1
 
-  server = osc_server.ThreadingOSCUDPServer(
-      (args.ip, args.port), dispatcher)
-  print("Serving on {}".format(server.server_address))
-  server.serve_forever()
+loop = asyncio.get_event_loop()
+server = osc_server.AsyncIOOSCUDPServer((args.ip, args.port), dispatcher, loop)
+print ("Serving on {}".format((args.ip, args.port)))
+server.serve()
+loop.run_forever()
