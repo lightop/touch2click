92335f83748745848fa30e929a71d779d32fb47c
Lightop <lightop@yandex.ru>
2018-01-12 20:58:54 +0300

    File opeining

--- ttcqt.py
+++ ttcqt.py
@@ -3,62 +3,10 @@
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+import yaml
 
+buttonData = []
 
-buttonData = [
-              ('ws1',50,68),
-              ('ws2',50,101),
-              ('ws3',50,134),
-              ('ws4',50,167),
-              ('ws5',50,200),
-              ('ws6',50,233),
-              ('ws7',50,266),
-              ('ws8',50,299),
-              ('ws9',50,332),
-              ('ws10',50,365),
-
-              ('pageplus', 45, 506),
-              ('pageminus',45, 678),
-
-              ('flash1',127,586),
-              ('flash2',206,586),
-              ('flash3',285,586),
-              ('flash4',364,586),
-              ('flash5',443,586),
-              ('flash6',522,586),
-              ('flash7',601,586),
-              ('flash8',680,586),
-              ('flash9',759,586),
-              ('flash10',838,586),
-
-              ('swop1',127,545),
-              ('swop2',206,545),
-              ('swop3',285,545),
-              ('swop4',364,545),
-              ('swop5',443,545),
-              ('swop6',522,545),
-              ('swop7',601,545),
-              ('swop8',680,545),
-              ('swop9',759,545),
-              ('swop10',838,545),
-
-              ('intensity',1029,538),
-              ('position',1071,538),
-              ('color',1113,538),
-              ('gobo',1155,538),
-              ('beam',1197,538),
-              ('effect',1239,538),
-              ('special',1281,538),
-              ('fx',1323,538),
-
-              ('encoder1up',1074,646),
-              ('encoder2up',1188,646),
-              ('encoder3up',1304,646),
-              ('encoder1down',1074,698),
-              ('encoder2down',1188,698),
-              ('encoder3down',1304,698),
-
-              ]
 
 class MainWindow(QMainWindow):
 	def __init__(self):
@@ -88,6 +36,13 @@ class MainWindow(QMainWindow):
 
 	def open(self):
 		filename, _ = QFileDialog.getOpenFileName(self)
+		print (filename)
+		with open (filename, 'r') as f:
+			data = yaml.load (f)
+			buttonData= data ['buttonData']
+			print (len(buttonData))
+			self.buttonTable.fillItems(buttonData)
+			
 
 	def save(self):
 		print ("Saved  "+str(self.times))
@@ -125,6 +80,8 @@ class MainWindow(QMainWindow):
 		self.fileMenu.addSeparator()
 		self.fileMenu.addAction(self.exitAct)
 
+		self.runMenu =self.menuBar().addMenu("&Run")
+
 class ButtonTab(QWidget):
 	def __init__(self):
 		super(QWidget,self).__init__()
@@ -138,15 +95,13 @@ class ButtonTab(QWidget):
 		layout.addWidget(self.table,0,0)
 
 
-	def fillItems(self):
+	def fillItems(self, buttonData):
 		length = len(buttonData)
 		self.table.setRowCount(length)
 		for row in range (length):
 			for column in range(3):
 				item = str(buttonData[row][column])
 				self.table.setItem(row, column, QTableWidgetItem(item))
-
-		super.statusBar().showMessage("Filled!!!!!!!!!!!")
 
 		
 
