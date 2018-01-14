a256cd4c1091112b8a82399e476c72b843b943d3
Lightop <lightop@yandex.ru>
2018-01-14 10:20:21 +0300

    Table populating complete

--- ttcqt.py
+++ ttcqt.py
@@ -6,6 +6,9 @@ from PyQt5.QtWidgets import *
 import yaml
 
 buttonData = []
+faderData =[]
+encoderData = []
+keyData = []
 
 
 class MainWindow(QMainWindow):
@@ -13,18 +16,21 @@ class MainWindow(QMainWindow):
 		super(MainWindow, self).__init__()
 
 		self.setWindowTitle("Touch2Click v0.01")
-		self.resize(400,600)
+		self.resize(550,600)
 
 		self.times = 0
 
 		self.buttonTable = ButtonTab()
+		self.faderTable = FaderTab()
+		self.encoderTable = EncoderTab()
+		self.keyTable = KeyTab()
 
 		self.tabwidget = QTabWidget()
 		self.tabwidget.addTab(self.buttonTable,"Buttons")
-		self.tabwidget.addTab(QLabel(""),"Faders")
-		self.tabwidget.addTab(QLabel(""),"Encoders")
-		self.tabwidget.addTab(QLabel(""),"Keys")
-		self.tabwidget.addTab(QLabel(""),"Grids")
+		self.tabwidget.addTab(self.faderTable,"Faders")
+		self.tabwidget.addTab(self.encoderTable,"Encoders")
+		self.tabwidget.addTab(self.keyTable,"Keys")
+		self.tabwidget.addTab(QLabel("Not yet implemented..."),"Grids")
 
 		self.setCentralWidget(self.tabwidget)
 
@@ -40,8 +46,14 @@ class MainWindow(QMainWindow):
 		with open (filename, 'r') as f:
 			data = yaml.load (f)
 			buttonData= data ['buttonData']
-			print (len(buttonData))
+			faderData = data ['faderData']
+			encoderData = data ['encoderData']
+			keyData = data ['keyData']
+			print (faderData)
 			self.buttonTable.fillItems(buttonData)
+			self.faderTable.fillItems(faderData)
+			self.encoderTable.fillItems(encoderData)
+			self.keyTable.fillItems(keyData)
 			
 
 	def save(self):
@@ -90,7 +102,8 @@ class ButtonTab(QWidget):
 		self.table = QTableWidget()
 		self.table.setRowCount (0)
 		self.table.setColumnCount (3)
-		self.table.setHorizontalHeaderLabels(['Name', 'X Click', 'Y Click',])
+		self.table.setHorizontalHeaderLabels(
+			['Index', 'X-Click', 'Y-Click',])
 
 		layout.addWidget(self.table,0,0)
 
@@ -103,7 +116,68 @@ class ButtonTab(QWidget):
 				item = str(buttonData[row][column])
 				self.table.setItem(row, column, QTableWidgetItem(item))
 
-		
+class FaderTab (QWidget):
+	def __init__(self):
+		super(QWidget,self).__init__()
+		layout = QGridLayout()
+		self.setLayout(layout)
+		self.table =QTableWidget()
+		self.table.setRowCount(0)
+		self.table.setColumnCount(5)
+		self.table.setHorizontalHeaderLabels(
+			['Index', 'X-Zero','Y-Zero','X-Full', 'Y-Full'])
+		layout.addWidget(self.table,0,0)
+	
+	def fillItems(self, faderData):
+		length = len(faderData)
+		self.table.setRowCount(length)
+		for row in range (length):
+			for column in range(5):
+				item = str(faderData[row][column])
+				print (item)
+				self.table.setItem(row, column, QTableWidgetItem(item))
+
+class EncoderTab (QWidget):
+	def __init__(self):
+		super(QWidget,self).__init__()
+		layout = QGridLayout()
+		self.setLayout(layout)
+		self.table =QTableWidget()
+		self.table.setRowCount(0)
+		self.table.setColumnCount(5)
+		self.table.setHorizontalHeaderLabels(
+			['Index', 'X-Center','Y-Center','X-Step', 'Y-Step'])
+		layout.addWidget(self.table,0,0)
+	
+	def fillItems(self, encoderData):
+		length = len(encoderData)
+		self.table.setRowCount(length)
+		for row in range (length):
+			for column in range(5):
+				item = str(encoderData[row][column])
+				print (item)
+				self.table.setItem(row, column, QTableWidgetItem(item))
+
+class KeyTab (QWidget):
+	def __init__(self):
+		super(QWidget,self).__init__()
+		layout = QGridLayout()
+		self.setLayout(layout)
+		self.table =QTableWidget()
+		self.table.setRowCount(0)
+		self.table.setColumnCount(2)
+		self.table.setHorizontalHeaderLabels(
+			['Index', 'Keys'])
+		layout.addWidget(self.table,0,0)
+	
+	def fillItems(self, keyData):
+		length = len(keyData)
+		self.table.setRowCount(length)
+		for row in range (length):
+			for column in range(2):
+				item = str(keyData[row][column])
+				print (item)
+				self.table.setItem(row, column, QTableWidgetItem(item))
 
 
 
